d171d7303fa23f8c1260ed5e0377e7aed6e88716
Enkhe Garmaev <garmaevenhe@gmail.com>
2024-02-16 16:31:10 +0900

    feat: add show user image

--- convert.js
+++ convert.js
@@ -1,3 +1,10 @@
+const UserImage = document.querySelector("#UserImage");
+const WebpImage = document.querySelector("#WebpImage");
+
 function convertImage(event) {
-  console.log("test");
+  if (event.target.files.length > 0) {
+    //show User image
+    let src = URL.createObjectURL(event.target.files[0]);
+    UserImage.src = src;
+  }
 }
